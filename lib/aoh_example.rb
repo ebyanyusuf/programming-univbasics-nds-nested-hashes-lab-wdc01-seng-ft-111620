cc34bd3b12084912a58e84bee3f6cbab88d4dc85
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-09 21:17:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,13 +7,16 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
+  new_array = [
+  {:name=>"Don Gately", :occupation=>"Live-in Staff"}, 
+  {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
+  {:name=>"Kate Gompert", :occupation=>"None"}, 
+  {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+  ]
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+ 
 end
 
 def aoh_lookup(aoh, row, key)
